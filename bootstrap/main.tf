99a7bb69d566affe7691e88f199a2d6bb07e25a7
Enrique Parada <pradapop78@gmail.com>
2025-12-07 04:45:02 -0500

    chore: enable versioning via aws_s3_bucket_versioning and harden state backend

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -28,6 +28,15 @@ resource "aws_s3_bucket" "tf_state" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
 resource "aws_s3_bucket_public_access_block" "tf_state_block" {
   bucket                  = aws_s3_bucket.tf_state.id
   block_public_acls       = true
